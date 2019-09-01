7102a4eac611ec43d332ac388f677e9dbce932b6
hao0731 <ss501801@gmail.com>
2019-09-01 21:24:32 +0800

    add bootstrap

---
+++ src/app/modules/shared/bootstrap/bootstrap.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { NgbButtonsModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        NgbButtonsModule
+    ],
+    exports: [
+        NgbButtonsModule
+    ]
+})
+export class BootstrapModule { }
