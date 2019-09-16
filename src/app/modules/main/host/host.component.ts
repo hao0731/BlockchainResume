7fbbf50963dd34188198086856dd9347f241d637
hao0731 <ss501801@gmail.com>
2019-09-16 23:39:49 +0800

    add dynamic form

--- src/app/modules/main/host/host.component.ts
+++ src/app/modules/main/host/host.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injector } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { ComponentBase } from 'src/app/base/component.base';
 
 @Component({
@@ -16,8 +16,31 @@ export class HostComponent extends ComponentBase {
     ) {
         super(injector);
         this.profileForm = this.formBuilder.group({
+            contract: ['', [Validators.required, this.addressValidator]],
             contact: ['', [Validators.required]],
-            autobiography: ['', [Validators.required]]
+            autobiography: ['', [Validators.required]],
+            skills: this.formBuilder.array([this.createSkillFields()])
+        });
+    }
+
+    public updateProfile(data: any): void {
+        console.log(data);
+    }
+
+    public addSkillField(): void {
+        const skills = this.profileForm.controls.skills as FormArray;
+        skills.push(this.createSkillFields());
+    }
+
+    public removeSkillField(index: number) {
+        const skills = this.profileForm.controls.skills as FormArray;
+        skills.removeAt(index);
+    }
+
+    private createSkillFields(): FormGroup {
+        return this.formBuilder.group({
+            class: ['', [Validators.required]],
+            name: ['', [Validators.required]]
         });
     }
 
--- src/app/modules/main/host/host.module.ts
+++ src/app/modules/main/host/host.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HostRoutingModule } from './host-routing.module';
 import { HostComponent } from './host.component';
 
 @NgModule({
-  declarations: [HostComponent],
-  imports: [
-    CommonModule,
-    HostRoutingModule
-  ]
+    declarations: [
+        HostComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HostRoutingModule
+    ]
 })
 export class HostModule { }
