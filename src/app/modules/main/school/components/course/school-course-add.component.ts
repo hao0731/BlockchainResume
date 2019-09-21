fe795265f3d2a9817cb46fefd24983de2e7b051b
hao0731 <ss501801@gmail.com>
2019-09-21 13:32:38 +0800

    add course and license

--- src/app/modules/main/school/components/course/school-course-add.component.ts
+++ src/app/modules/main/school/components/course/school-course-add.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ComponentBase } from 'src/app/base/component.base';
 
 @Component({
-  selector: 'app-school-course-add',
-  templateUrl: './school-course-add.component.html',
-  styleUrls: ['./school-course-add.component.scss']
+    selector: 'app-school-course-add',
+    templateUrl: './school-course-add.component.html',
+    styleUrls: ['./school-course-add.component.scss']
 })
-export class SchoolCourseAddComponent implements OnInit {
+export class SchoolCourseAddComponent extends ComponentBase {
+    public courseForm: FormGroup;
 
-  constructor() { }
+    constructor(
+        private injector: Injector,
+        private formBuilder: FormBuilder
+    ) {
+        super(injector);
+        this.courseForm = this.formBuilder.group({
+            contract: ['', [Validators.required, this.addressValidator]],
+            name: ['', [Validators.required]],
+            content: ['', [Validators.required]],
+            comment: ['', [Validators.required]],
+            grade: ['', [Validators.required, Validators.pattern(/^(?:[1-9]?\d|100)$/)]]
+        });
+    }
 
-  ngOnInit() {
-  }
+    public addCourse(data: any): void {
+        this.isPending = true;
+        this.setFormDisabled(this.courseForm);
+        const resume = this.providerSvc.getResume(data.contract);
+        resume.methods.setCourse(data.name, data.content, data.comment, data.grade)
+        .send({ from: this.providerSvc.defaultAccount })
+        .on('receipt', receipt => {
+            this.transactionConfirmed();
+            this.courseForm.reset();
+            this.setFormDisabled(this.courseForm, false);
+        })
+        .on('error', err => {
+            this.transactionError();
+            this.courseForm.reset();
+            this.setFormDisabled(this.courseForm, false);
+        });
+    }
 
 }
--- src/app/modules/main/school/components/license/school-license-add.component.ts
+++ src/app/modules/main/school/components/license/school-license-add.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { ComponentBase } from 'src/app/base/component.base';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-school-license-add',
-  templateUrl: './school-license-add.component.html',
-  styleUrls: ['./school-license-add.component.scss']
+    selector: 'app-school-license-add',
+    templateUrl: './school-license-add.component.html',
+    styleUrls: ['./school-license-add.component.scss']
 })
-export class SchoolLicenseAddComponent implements OnInit {
+export class SchoolLicenseAddComponent extends ComponentBase {
+    public licenseForm: FormGroup;
 
-  constructor() { }
+    constructor(
+        private injector: Injector,
+        private formBuilder: FormBuilder
+    ) {
+        super(injector);
+        this.licenseForm = this.formBuilder.group({
+            contract: ['', [Validators.required, this.addressValidator]],
+            name: ['', [Validators.required]],
+            content: ['', [Validators.required]]
+        });
+    }
 
-  ngOnInit() {
-  }
+    public addLicense(data: any): void {
+        const resume = this.providerSvc.getResume(data.contract);
+        resume.methods.setLicense(data.name, data.content)
+        .send({ from: this.providerSvc.defaultAccount })
+        .on('receipt', receipt => {
+            this.transactionConfirmed();
+            this.licenseForm.reset();
+            this.setFormDisabled(this.licenseForm, false);
+        })
+        .on('error', err => {
+            this.transactionError();
+            this.licenseForm.reset();
+            this.setFormDisabled(this.licenseForm, false);
+        });
+    }
 
 }
